f6d21ba0fd2b6e7139917b44a374b37f19dbac9f
Suman Prasad <sumanprasad9766@gmail.com>
2023-02-20 12:08:46 +0530

    made changes to program.cs file

--- AWSCodePipelineDemo/Program.cs
+++ AWSCodePipelineDemo/Program.cs
@@ -1,26 +1,39 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace MyApp
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello, World!");
+                });
+                endpoints.MapControllers();
+            });
+        }
+    }
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
 app.MapGet("/", () => "Hello .NET Team, Welcome to 2023" +
 "Here, we are exploring our .NET Project & Hosted using AWS CodePipeline");
 
