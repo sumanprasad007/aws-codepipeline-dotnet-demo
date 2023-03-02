b475009b7120b642eb962d673ce931f3bb09b5e5
Suman Prasad <sumanprasad9766@gmail.com>
2023-03-02 11:46:41 +0530

    modified on 2nd March

--- AWSCodePipelineDemo/Program.cs
+++ AWSCodePipelineDemo/Program.cs
@@ -22,7 +22,7 @@ app.UseAuthorization();
 
 app.MapControllers();
 app.MapGet("/", () => "Hello .NET Team, Welcome to 2023" +
-" Here, we are exploring our .NET Project & Hosted ");
+" Here, we are exploring our .NET Project & Hosted");
 
 app.Run();
 
